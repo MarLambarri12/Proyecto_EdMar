e30860f12e913990e5cb2306beda6747e9ab3ca2
MarLambarri12 <marialambarri12@gmail.com>
2024-08-01 16:28:36 -0600

    Se añadio la estructura principal del proyecto

---
+++ dependencias.php
@@ -0,0 +1,4 @@
+<?php 
+echo '<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">';
+echo '<script src="bootstrap/js/bootstrap.bundle.min.js"></script>';
+?>
--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>EdMar</title>
+    <?php include 'dependencias.php';?>
+    <?php include 'Vistas/Menu.php';?>
 </head>
 <body>
-    <h1>Hola</h1>
+    
 </body>
 </html>
