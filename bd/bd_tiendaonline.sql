1b1bfc75eccab95f41376041289f35afd9997e31
MarLambarri12 <marialambarri12@gmail.com>
2024-08-08 19:51:15 -0600

    BD Agregada

--- bd/bd_tiendaonline.sql
+++ bd/bd_tiendaonline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-08-2024 a las 08:15:51
+-- Tiempo de generación: 09-08-2024 a las 03:40:46
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.11
 
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bd_tiendaonline`
 --
+CREATE DATABASE IF NOT EXISTS `bd_tiendaonline` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `bd_tiendaonline`;
 
 -- --------------------------------------------------------
 
@@ -28,8 +30,9 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `apellido_paterno` varchar(50) NOT NULL,
   `apellido_materno` varchar(50) NOT NULL,
@@ -37,36 +40,20 @@ CREATE TABLE `usuarios` (
   `sexo` enum('Masculino','Femenino','Indeciso') NOT NULL,
   `fecha_nacimiento` date NOT NULL,
   `email` varchar(100) NOT NULL,
-  `contraseña` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `pass` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido_paterno`, `apellido_materno`, `usuario`, `sexo`, `fecha_nacimiento`, `email`, `contraseña`) VALUES
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido_paterno`, `apellido_materno`, `usuario`, `sexo`, `fecha_nacimiento`, `email`, `pass`) VALUES
 (1, 'Edgar Adrián', 'Preciado', 'Naranjo', 'EdgarAd', 'Masculino', '1997-11-09', 'edgar_@gmail.com', '$2y$10$iDFHZ06gjrY9fEy8iaBg7O9fRZV.CxmIdr2F7w3x1acLmj9OKZhvq'),
-(2, 'María', 'Lambarri', 'Hernández', 'Mar', 'Femenino', '1997-12-07', 'mlambarri3@gmail.com', '$2y$10$borZuxhPyEsG59AjjAu1XOcYNz2YgfHj5E5XHXhYqOsBVqmnRXbdK');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+(2, 'María', 'Lambarri', 'Hernández', 'Mar', 'Femenino', '1997-12-07', 'mlambarri3@gmail.com', '$2y$10$borZuxhPyEsG59AjjAu1XOcYNz2YgfHj5E5XHXhYqOsBVqmnRXbdK'),
+(3, 'Matias', 'Almeyda', 'Lozano', 'matichiva', 'Masculino', '1995-08-05', 'mati@gmail.com', '$2y$10$IQe/QWFzDhPd2H5FMRM1KeD6c9n7nlV/.FVxZKx6g7dsU/VvBQIjG'),
+(5, 'María', 'Lambarri', 'Hernandez', 'Admin', 'Femenino', '1997-12-07', 'admin@gmail.com', '$2y$10$HaUBAizKuMQ9GTQwQHdT3OMsEJU3Pipmjg7QcJ52oyXRSoaV6Hxfy'),
+(6, 'Brenda', 'Lambarri', 'Hernandez', 'Usuario', 'Femenino', '2018-02-01', 'usuario@gmail.com', '$2y$10$Z.ho81ucBjI53Wa/rF97f.ZhcsSo8g..5nPs8Fjmt3q4wlu8Ugb9e');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
